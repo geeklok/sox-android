14efc3ce6cc48c195248f71f044db72a7a73ebb9
geeklok <geeklok@outlook.com>
2017-03-18 11:36:13 +0800

    seperate static libraries

--- build/build.sh
+++ build/build.sh
@@ -30,6 +30,8 @@ for arch in armeabi armeabi-v7a arm64-v8a x86; do
     armeabi)
         SYSTEM_VERSION=15
         ANDROID_ARCH_ABI=armeabi
+        NEON_ON=OFF
+        ARM_MODE=ON
         SYSROOT=$SYSTEM_VERSION/arch-arm
         C_COMPILER=arm-linux-androideabi-4.9/prebuilt/$HOST_SYSTEM/bin/arm-linux-androideabi
         C_FLAGS=-DALIGNBYTES=3
@@ -38,6 +40,8 @@ for arch in armeabi armeabi-v7a arm64-v8a x86; do
     armeabi-v7a)
         SYSTEM_VERSION=15
         ANDROID_ARCH_ABI=armeabi-v7a
+        NEON_ON=ON
+        ARM_MODE=ON
         SYSROOT=$SYSTEM_VERSION/arch-arm
         C_COMPILER=arm-linux-androideabi-4.9/prebuilt/$HOST_SYSTEM/bin/arm-linux-androideabi
         C_FLAGS=-DALIGNBYTES=3
@@ -46,6 +50,8 @@ for arch in armeabi armeabi-v7a arm64-v8a x86; do
     arm64-v8a)
         SYSTEM_VERSION=21
         ANDROID_ARCH_ABI=arm64-v8a
+        NEON_ON=OFF
+        ARM_MODE=OFF
         SYSROOT=$SYSTEM_VERSION/arch-arm64
         C_COMPILER=aarch64-linux-android-4.9/prebuilt/$HOST_SYSTEM/bin/aarch64-linux-android
         C_FLAGS=-DALIGNBYTES=7
@@ -54,6 +60,8 @@ for arch in armeabi armeabi-v7a arm64-v8a x86; do
     x86)
         SYSTEM_VERSION=15
         ANDROID_ARCH_ABI=x86
+        NEON_ON=OFF
+        ARM_MODE=OFF
         SYSROOT=$SYSTEM_VERSION/arch-x86
         C_COMPILER=x86-4.9/prebuilt/$HOST_SYSTEM/bin/i686-linux-android
         C_FLAGS=-DALIGNBYTES=3
@@ -66,12 +74,20 @@ ${CMAKE} . \
   -DCMAKE_SYSTEM_NAME=Android \
   -DCMAKE_SYSTEM_VERSION=$SYSTEM_VERSION \
   -DCMAKE_ANDROID_ARCH_ABI=$ANDROID_ARCH_ABI \
+  -DCMAKE_ANDROID_ARM_NEON=$NEON_ON \
+  -DCMAKE_ANDROID_ARM_MODE=$ARM_MODE \
   -DCMAKE_SYSROOT=$ANDROID_NDK/platforms/android-$SYSROOT \
   -DCMAKE_C_COMPILER=$ANDROID_NDK/toolchains/$C_COMPILER-gcc \
-  -DCMAKE_AR=$ANDROID_NDK/toolchains/$C_COMPILER-ar \
   -DCMAKE_C_FLAGS="-DHAVE_FMEMOPEN=TRUE -std=c99 -fopenmp $C_FLAGS"
 
 make sox-android
+
+mkdir -p libs/$arch
+cp libsox-android.a libs/$arch
+cp ../sox/src/liblibsox.a libs/$arch
+cp ../sox/lpc10/liblpc10.a libs/$arch
+cp ../sox/libgsm/libgsm.a libs/$arch
+
 make clean
 rm -rf CMakeCache.txt CMakeFiles/
 
